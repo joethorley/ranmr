d7cb37bb0ab0c121995027a661642fc2d03b5d6a
joethorley <joe@poissonconsulting.ca>
2015-09-17 16:00:33 -0700

    added code to fill in missing ages

--- R/data.R
+++ R/data.R
@@ -1,3 +1,16 @@
+fill_in_ages <- function(x) {
+  assert_that(!is.unsorted(x$Date))
+  age <- x$Age
+  year <- lubridate::year(x$Date)
+  newage <- age[1] + year - year[1]
+  age[is.na(age)] <- newage[is.na(age)]
+  if (!identical(age, newage)) {
+    warning("inconsistent ages fish ", as.character(x$Fish[1]), call. = FALSE)
+  }
+  x$Age <- newage
+  x
+}
+
 #' Ferox Trout Mark-Recapture Data
 #'
 #' @return A data.frame of ferox trout mark-recapture data from
@@ -6,7 +19,9 @@
 #' @export
 #' @examples
 #' ferox()
-ferox <- function () {
+ferox <- function() {
   data(list = "ferox", package = "ranmrdata", envir = environment())
+
+  ferox %<>% plyr::ddply(.variables = ("Fish"), fill_in_ages)
   ferox
 }
